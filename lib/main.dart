516d23d515b54010b532ccc63a9292e83dcf9c19
Updesh Pr. Singh <updesh071@gmail.com>
2023-06-03 18:47:41 +0530

    day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/Pages/login_page.dart';
 
-import 'home_page.dart';
+import 'Pages/home_page.dart';
+import 'Pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  // override means change in predefine function(Method).....of StatelessWidget
   @override
   Widget build(BuildContext context) {
-    
-
+    // BuildContext define by user as widget is tree structure, so it define location of elements like homepase,etc.
     return MaterialApp(
-      home: HomePage(),
+      // home: HomePage(),
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      themeMode: ThemeMode.light,
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage()
+      },
     );
   }
 }
